774c7a4b180e73db8f2febf4ab46e29fdd5d9e2b
Meet Rajput <rajputmeet10@gmail.com>
2022-03-19 23:19:26 +0530

    frontEnd-(message)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 function App() {
   return (
     <div className="app">
       <Sidebar/>
+      <Chat/>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Message.css';
+
+function Message() {
+  return (
+    <div className='message'>
+        <h3>I am a message</h3>
+    </div>
+  )
+}
+
+export default Message
