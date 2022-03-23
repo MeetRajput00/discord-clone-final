a02949164567c0a5d6b28140bdcc5a9a557829dc
Meet Rajput <rajputmeet10@gmail.com>
2022-03-23 22:51:39 +0530

    sidebarChannel added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
@@ -12,9 +12,30 @@ import HeadsetIcon from '@material-ui/icons/Headset';
 import SettingsIcon from '@material-ui/icons/Settings';
 import {useSelector} from 'react-redux';
 import {selectUser} from './features/userSlice';
-import {auth} from './firebase'
+import {auth} from './firebase';
+import db from './firebase';
 function Sidebar() {
     const user=useSelector(selectUser);
+    const [channels,setChannels]=useState([]);
+    useEffect(() => {
+        db.collection("channels").onSnapshot((snapshot) =>
+          setChannels(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              channel: doc.data(),
+            }))
+          )
+        );
+      }, []);
+      const handleAddChannel = () => {
+        const channelName = prompt("Enter a new channel name");
+    
+        if (channelName) {
+          db.collection("channels").add({
+            channelName: channelName,
+          });
+        }
+      };
   return (
     <div className='sidebar'>
 
@@ -29,13 +50,16 @@ function Sidebar() {
                     <ExpandMoreIcon/>
                     <h4>Text Channels</h4>
                 </div>
-                <AddIcon className='sidebar_addChannel'/>
+                <AddIcon onClick={handleAddChannel} className='sidebar_addChannel'/>
             </div>
             <div className="sidebar_channelsList">
-                <SidebarChannel/>
-                <SidebarChannel/>
-                <SidebarChannel/>
-                <SidebarChannel/>
+            {channels.map(({ id, channel }) => (
+            <SidebarChannel
+              key={id}
+              id={id}
+              channelName={channel.channelName}
+            />
+          ))}
             </div>
         </div>
         <div className="sidebar_voice">
--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './SidebarChannel.css';
 
-function SidebarChannel({id, channel}) {
+function SidebarChannel({id, channelName}) {
   return (
     <div className='SidebarChannel'>
-        <h4><span classname="sidebarChannel_hash">#</span>Youtube</h4>
+        <h4><span classname="sidebarChannel_hash">#</span>{channelName}</h4>
     </div>
   )
 }
